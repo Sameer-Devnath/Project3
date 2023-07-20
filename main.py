33a7fc526c51e11e3ff20fcf49aa306a9a39e09c
Sameer Devnath <139816841+Sameer-Devnath@users.noreply.github.com>
2023-07-20 22:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,17 +19,3 @@ print("Downloading...")
 videos[stream].download()
 print("Successfully")
 
-from pytube import YouTube
-
-def Download(link):
-    youtubeObject = YouTube(link)
-    youtubeObject = youtubeObject.streams.get_highest_resolution()
-    try:
-        youtubeObject.download()
-    except:
-        print("An error has occurred")
-    print("Download is completed successfully")
-
-
-link = input("Enter the YouTube video URL: ")
-Download(link)
